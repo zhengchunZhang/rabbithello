1b43ba86f1d9928cf762f8b7333112619e044cf0
zhangzhengchun <1437237338@qq.com>
2023-09-24 11:47:52 +0800

    模拟优先级队列

--- com.atguigu.rabbitmqOne/src/main/java/com/atguigu/rabbitmq/one/Producer.java
+++ com.atguigu.rabbitmqOne/src/main/java/com/atguigu/rabbitmq/one/Producer.java
@@ -1,10 +1,10 @@
 package com.atguigu.rabbitmq.one;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 
@@ -13,7 +13,7 @@ public class Producer {
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("192.168.154.155");
+        factory.setHost("192.168.154.159");
         factory.setUsername("admin");
 
         factory.setPassword("123");
@@ -26,10 +26,20 @@ public class Producer {
          * 3。该队列时候只供一个消费者进行消费 是否进行消息共享 true表示可以多个消费者消费 false表示单个消费者
          * 4.是否自动删除，最后一个消费者断开连接以后 该队列是否自动删除
          */
+        Map<String,Object> arguments = new HashMap<>();
+        arguments.put("x-max-priority",10);//官方允许是0-255之间 允许设置优先级范围是0-10 不要设置太大 兰妃cpu
 
+        channel.queueDeclare(QUEUE_NAME,true,false,false,arguments);
+        for (int i = 1; i < 11; i++) {
+            String message = "hello world" + i;
+            if(i ==5) {
+                AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().priority(5).build();
+                channel.basicPublish("",QUEUE_NAME,properties,message.getBytes());
+            } else {
+                channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            }
+        }
 
-        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
-        String message = "hello world";
         /**
          * 发送一个消费
          * 1.发送到那个交换机
@@ -37,28 +47,8 @@ public class Producer {
          * 3.其他参数
          * 4.消息体
          */
-        channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+//        channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
         System.out.println("消息发送完毕");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
